2fb6d628f63941b244f7416026ed4b785bfea593
Ronald Gerits <ronald.gerits@gmail.com>
2023-02-22 22:14:52 +0100

    Insert CSS

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,12 @@
 <x-layout>
+    @include('_posts-header')
 
-    @foreach($posts as $post)
-        <article class="{{ $loop->even ? 'mb-6' : '' }}">
-            <h1>
-                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
-            </h1>
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-            <div>
-                <p>{{ $post->excerpt }}</p>
-            </div>
-        </article>
-    @endforeach
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    @if($posts->count())
+        <x-posts-grid :posts="$posts"/>
+    @else
+        <p class="text-center">No posts yet, come back later.</p>
+    @endif
+
+</main>
 </x-layout>
-
-
-
-
