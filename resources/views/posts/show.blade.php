87572819f4e79ad8643f7aa6bb1a4f831668e7f0
Ronald Gerits <ronald.gerits@gmail.com>
2023-04-11 20:21:35 +0200

    Episode 41

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,7 +13,9 @@
                 <div class="flex items-center lg:justify-center text-sm mt-4">
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3 text-left">
-                        <h5 class="font-bold">{{ $post->author->name }}</h5>
+                        <h5 class="font-bold">
+                            <a href="?author={{ $post->author->username }}">{{  $post->author->name }}</a>
+                        </h5>
                     </div>
                 </div>
             </div>
@@ -22,7 +24,7 @@
                 <div class="hidden lg:flex justify-between mb-6">
                     <a href="/"
                        class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
-                        <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
+                    <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
                             <g fill="none" fill-rule="evenodd">
                                 <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
                                 </path>
